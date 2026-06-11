0643b4378688b738dc32c9d4cbaaebb6a4a17577
arthur werlang <arthurceschini2009@gmail.com>
2026-06-10 21:50:47 -0300

    par e impar

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//  eSCREVA A MENSAGEM "INFORME UM NUMERO"
+Console.Write("informe um numero: ");
+// CRIAAVARIAVELQUESALVAUMNUMERO, LEIA OQ O USUARIO DIGITAR
+// E TRANSFORMA O VALORLIDO DE TEXTO PARA INTEIRO (INT.PARSE)
+int variavelquesalvaumnumero = int.Parse(Console.ReadLine()!);
+
+// VERIFICA SE O VALOR DA VARIAVEL QUE SALVA O NUMERO E PAR
+if (variavelquesalvaumnumero % 2 ==0)
+{
+    Console.WriteLine($"numero {variavelquesalvaumnumero} é par"); //verifica a variavel e diz que é par
+} else { //se não for par
+    Console.WriteLine($"numero {variavelquesalvaumnumero} é ímpar"); //diz que é impar
+} // fim se
+
