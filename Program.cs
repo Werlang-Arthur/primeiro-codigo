3baab925ecd657e5ade47f50859f66987a95cdcf
arthur werlang <arthurceschini2009@gmail.com>
2026-06-10 22:20:39 -0300

    CONTADOR

--- Program.cs
+++ Program.cs
@@ -1,14 +1,12 @@
-﻿//  eSCREVA A MENSAGEM "INFORME UM NUMERO"
-Console.Write("informe um numero: ");
-// CRIAAVARIAVELQUESALVAUMNUMERO, LEIA OQ O USUARIO DIGITAR
-// E TRANSFORMA O VALORLIDO DE TEXTO PARA INTEIRO (INT.PARSE)
-int variavelquesalvaumnumero = int.Parse(Console.ReadLine()!);
+﻿using System;
 
-// VERIFICA SE O VALOR DA VARIAVEL QUE SALVA O NUMERO E PAR
-if (variavelquesalvaumnumero % 2 ==0)
+class Program
 {
-    Console.WriteLine($"numero {variavelquesalvaumnumero} é par"); //verifica a variavel e diz que é par
-} else { //se não for par
-    Console.WriteLine($"numero {variavelquesalvaumnumero} é ímpar"); //diz que é impar
-} // fim se
-
+    static void Main()
+    {
+        for (int numero = 1; numero <= 5; numero++)
+        {
+            Console.WriteLine(numero);
+        }
+    }
+}
